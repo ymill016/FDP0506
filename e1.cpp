81d4aac07acb318ceab43affab4f136a67734427
ymill016 <00083924@uca.edu.sv>
2025-06-05 10:16:34 -0600

    hola

--- e1.cpp
+++ e1.cpp
@@ -1,1 +1,23 @@
-//aa
+/*Tarifa de Luz Eléctrica
+Calcula la tarifa según el consumo: 
+menor a 100 kWh, tarifa baja; entre 100 y 200 kWh, 
+tarifa media; más de 200 kWh, tarifa alta.
+if-esle 19*/
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int consumoEnergia;
+    int tarifa = 10; 
+
+    cout << "Tarifa de luz electrica\n";
+    cout << "Dame el dato de tu consumo de energia: ";
+    cin >> consumoEnergia;
+
+    if (consumoEnergia < 100){
+        cout << "Has consumido " << consumoEnergia << "kwh\n";
+        cout << "Tu tarifa a pagar es baja: " << "$"<< tarifa;
+    }else if (consumoenr)
+    return 0;
+}
